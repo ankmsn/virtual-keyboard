94b2af2e7124aafd655e3e8ee17a950460388259
ankmsn <khomstyle@gmail.com>
2023-04-29 19:40:19 +0100

    fix: files js by eslint

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-import {lettersEng, lettersEngShift, lettersEngCaps, lettersUkr, lettersUkrShift, lettersUkrCaps} from '/data.js'
+import {
+  lettersEng, lettersEngShift, lettersEngCaps, lettersUkr, lettersUkrShift, lettersUkrCaps,
+} from './data.js'; // eslint-disable-line import/extensions
 
 document.body.insertAdjacentHTML('afterbegin', `
 <div class="wrapper">
@@ -20,136 +22,148 @@ const grid = document.querySelector('.grid');
 let letters;
 let lettersShift;
 let lettersCaps;
-let langText = document.querySelector('.top-text > p:last-child');
-let langInstruction = document.querySelector('.wrapper > p');
+const langText = document.querySelector('.top-text > p:last-child');
+const langInstruction = document.querySelector('.wrapper > p');
 
-function selectEng () {
+function selectEng() {
   letters = lettersEng;
   lettersShift = lettersEngShift;
   lettersCaps = lettersEngCaps;
-  langText.textContent = 'ENG'
-  langInstruction.textContent = 'Press Shift + Alt to switch language'
-  localStorage.setItem("language", "ENG");
-} 
+  langText.textContent = 'ENG';
+  langInstruction.textContent = 'Press Shift + Alt to switch language';
+  localStorage.setItem('language', 'ENG');
+}
 
-function selectUkr () {
+function selectUkr() {
   letters = lettersUkr;
   lettersShift = lettersUkrShift;
   lettersCaps = lettersUkrCaps;
   langText.textContent = 'УКР';
-  langInstruction.textContent = 'Натисніть Shift + Alt щоб змінити мову'
-  localStorage.setItem("language", "UKR");
-} 
-
-if (localStorage.getItem("language") === 'ENG' || !localStorage.getItem("language")) {
-  selectEng ();
-} else {
-  selectUkr ();
+  langInstruction.textContent = 'Натисніть Shift + Alt щоб змінити мову';
+  localStorage.setItem('language', 'UKR');
 }
 
-  letters.forEach(letter => {
-    const cell = document.createElement('div');
-    cell.textContent = letter;
-    cell.classList.add('cell');
-    grid.appendChild(cell);
-  });
+if (localStorage.getItem('language') === 'ENG' || !localStorage.getItem('language')) {
+  selectEng();
+} else {
+  selectUkr();
+}
 
-  const cells = document.querySelectorAll('.cell');
-  const textarea = document.getElementById('area');
-  let isCapsLockOn = false;
+letters.forEach((letter) => {
+  const cell = document.createElement('div');
+  cell.textContent = letter;
+  cell.classList.add('cell');
+  grid.appendChild(cell);
+});
 
-  const keys = {
-    'Backquote': cells[0],
-    'Digit1': cells[1],
-    'Digit2': cells[2],
-    'Digit3': cells[3],
-    'Digit4': cells[4],
-    'Digit5': cells[5],
-    'Digit6': cells[6],
-    'Digit7': cells[7],
-    'Digit8': cells[8],
-    'Digit9': cells[9],
-    'Digit0': cells[10],
-    'Minus': cells[11],
-    'Equal': cells[12],
-    'Backspace': cells[13],
-    'Tab': cells[14],
-    'KeyQ': cells[15],
-    'KeyW': cells[16],
-    'KeyE': cells[17],
-    'KeyR': cells[18],
-    'KeyT': cells[19],
-    'KeyY': cells[20],
-    'KeyU': cells[21],
-    'KeyI': cells[22],
-    'KeyO': cells[23],
-    'KeyP': cells[24],
-    'BracketLeft': cells[25],
-    'BracketRight': cells[26],
-    'Backslash': cells[27],
-    'Delete': cells[28],
-    'KeyA': cells[29],
-    'KeyS': cells[30],
-    'CapsLock': cells[31],
-    'KeyD': cells[32],
-    'KeyF': cells[33],
-    'KeyG': cells[34],
-    'KeyH': cells[35],
-    'KeyJ': cells[36],
-    'KeyK': cells[37],
-    'KeyL': cells[38],
-    'Semicolon': cells[39],
-    'Quote': cells[40],
-    'KeyZ': cells[41],
-    'KeyX': cells[42],
-    'Enter': cells[43],
-    'ShiftLeft': cells[44],
-    'KeyC': cells[45],
-    'KeyV': cells[46],
-    'KeyB': cells[47],
-    'KeyN': cells[48],
-    'KeyM': cells[49],
-    'Comma': cells[50],
-    'Period': cells[51],
-    'Slash': cells[52],
-    'ArrowUp': cells[53],
-    'ControlLeft': cells[54],
-    'MetaLeft': cells[55],
-    'ShiftRight': cells[56],
-    'AltLeft': cells[57],
-    'AltRight': cells[58],
-    'ArrowLeft': cells[59],
-    'ArrowDown': cells[60],
-    'Space': cells[61],
-    'ArrowRight': cells[62],
-    'ControlRight': cells[63]
+const cells = document.querySelectorAll('.cell');
+const textarea = document.getElementById('area');
+let isCapsLockOn = false;
+
+const keys = {
+  Backquote: cells[0],
+  Digit1: cells[1],
+  Digit2: cells[2],
+  Digit3: cells[3],
+  Digit4: cells[4],
+  Digit5: cells[5],
+  Digit6: cells[6],
+  Digit7: cells[7],
+  Digit8: cells[8],
+  Digit9: cells[9],
+  Digit0: cells[10],
+  Minus: cells[11],
+  Equal: cells[12],
+  Backspace: cells[13],
+  Tab: cells[14],
+  KeyQ: cells[15],
+  KeyW: cells[16],
+  KeyE: cells[17],
+  KeyR: cells[18],
+  KeyT: cells[19],
+  KeyY: cells[20],
+  KeyU: cells[21],
+  KeyI: cells[22],
+  KeyO: cells[23],
+  KeyP: cells[24],
+  BracketLeft: cells[25],
+  BracketRight: cells[26],
+  Backslash: cells[27],
+  Delete: cells[28],
+  KeyA: cells[29],
+  KeyS: cells[30],
+  CapsLock: cells[31],
+  KeyD: cells[32],
+  KeyF: cells[33],
+  KeyG: cells[34],
+  KeyH: cells[35],
+  KeyJ: cells[36],
+  KeyK: cells[37],
+  KeyL: cells[38],
+  Semicolon: cells[39],
+  Quote: cells[40],
+  KeyZ: cells[41],
+  KeyX: cells[42],
+  Enter: cells[43],
+  ShiftLeft: cells[44],
+  KeyC: cells[45],
+  KeyV: cells[46],
+  KeyB: cells[47],
+  KeyN: cells[48],
+  KeyM: cells[49],
+  Comma: cells[50],
+  Period: cells[51],
+  Slash: cells[52],
+  ArrowUp: cells[53],
+  ControlLeft: cells[54],
+  MetaLeft: cells[55],
+  ShiftRight: cells[56],
+  AltLeft: cells[57],
+  AltRight: cells[58],
+  ArrowLeft: cells[59],
+  ArrowDown: cells[60],
+  Space: cells[61],
+  ArrowRight: cells[62],
+  ControlRight: cells[63],
+};
+
+function updateKeysContent(baseLetters) {
+  for (let i = 0; i < baseLetters.length; i += 1) {
+    cells[i].textContent = baseLetters[i];
   }
+}
 
-  function updateKeysContent(letters) {
-      for (let i=0; i<letters.length; i++){
-        cells[i].textContent = letters[i]
-     }
-   }
+function highlightCell(cell) {
+  const cellStyle = cell.style;
+  cellStyle.backgroundColor = 'lightblue';
+  cellStyle.transform = 'scale(0.9)';
+}
+
+function unhighlightCell(cell) {
+  const cellStyle = cell.style;
+  cellStyle.backgroundColor = '';
+  cellStyle.transform = '';
+}
 
 document.addEventListener('keydown', (event) => {
   const key = event.code;
-  const content = keys[key].textContent
-    
+  const content = keys[key].textContent;
+
   if (key in keys) {
     keys[key].style.backgroundColor = 'lightblue';
     keys[key].style.transform = 'scale(0.9)';
   }
 
-  if (event.shiftKey && event.altKey){
-    if (localStorage.getItem("language") === 'ENG' || !localStorage.getItem("language")){
-     selectUkr ()
-       } else {
-     selectEng ()
-      }
-   }
+  if (event.shiftKey && event.altKey) {
+    if (localStorage.getItem('language') === 'ENG' || !localStorage.getItem('language')) {
+      selectUkr();
+    } else {
+      selectEng();
+    }
+  }
 
   if (key === 'ShiftRight' || key === 'ShiftLeft') {
-     updateKeysContent(lettersShift); 
+    updateKeysContent(lettersShift);
   }
 
   if (key === 'CapsLock') {
@@ -171,55 +185,54 @@ document.addEventListener('keydown', (event) => {
   if (key === 'Tab') {
     event.preventDefault();
     const cursorPos = textarea.selectionStart;
-    textarea.value = textarea.value.slice(0, cursorPos) + '\t' + textarea.value.slice(cursorPos);
+    textarea.value = `${textarea.value.slice(0, cursorPos)}\t${textarea.value.slice(cursorPos)}`;
     textarea.selectionStart = cursorPos + 1;
     textarea.selectionEnd = cursorPos + 1;
   }
 
   if (key === 'Enter') {
     const cursorPos = textarea.selectionStart;
-    textarea.value = textarea.value.slice(0, cursorPos) + '\n' + textarea.value.slice(cursorPos);
+    textarea.value = `${textarea.value.slice(0, cursorPos)}\n${textarea.value.slice(cursorPos)}`;
     textarea.selectionStart = cursorPos + 1;
     textarea.selectionEnd = cursorPos + 1;
   }
 
   if (key === 'Backspace') {
-    textarea.value = textarea.value.slice(0, -1);  
+    textarea.value = textarea.value.slice(0, -1);
   }
 
   if (key === 'Delete' && textarea.selectionStart < textarea.value.length) {
-    textarea.value = textarea.value.slice(0, -1);  
+    textarea.value = textarea.value.slice(0, -1);
   }
 
   if (key === 'ArrowLeft') {
     textarea.selectionStart -= 1;
-    textarea.selectionEnd = textarea.selectionStart +1;   
+    textarea.selectionEnd = textarea.selectionStart + 1;
     textarea.focus();
-  } 
+  }
 
   if (key === 'ArrowRight') {
     textarea.selectionStart += 1;
-    textarea.selectionEnd = textarea.selectionStart +1;   
+    textarea.selectionEnd = textarea.selectionStart + 1;
     textarea.focus();
-  } 
+  }
 
   if (key === 'ArrowDown') {
-    const currentCursorPosition = textarea.selectionStart
-    const numberInCurrentString = currentCursorPosition - textarea.value.lastIndexOf('\n', currentCursorPosition)
+    const currentCursorPosition = textarea.selectionStart;
+    const numberInCurrentString = currentCursorPosition - textarea.value.lastIndexOf('\n', currentCursorPosition);
     textarea.selectionStart = textarea.value.indexOf('\n', currentCursorPosition) + numberInCurrentString;
-    textarea.selectionEnd = textarea.selectionStart +1; 
+    textarea.selectionEnd = textarea.selectionStart + 1;
     textarea.focus();
-  } 
+  }
 
   if (key === 'ArrowUp') {
-    const currentCursorPosition = textarea.selectionStart
-    const prevStringEnd = textarea.value.lastIndexOf('\n', currentCursorPosition)
-    const numberInCurrentString = currentCursorPosition - prevStringEnd
-    textarea.selectionStart = textarea.value.lastIndexOf('\n', prevStringEnd-1) + numberInCurrentString;
-    textarea.selectionEnd = textarea.selectionStart +1; 
+    const currentCursorPosition = textarea.selectionStart;
+    const prevStringEnd = textarea.value.lastIndexOf('\n', currentCursorPosition);
+    const numberInCurrentString = currentCursorPosition - prevStringEnd;
+    textarea.selectionStart = textarea.value.lastIndexOf('\n', prevStringEnd - 1) + numberInCurrentString;
+    textarea.selectionEnd = textarea.selectionStart + 1;
     textarea.focus();
-  } 
-
+  }
 });
 
 document.addEventListener('keyup', (event) => {
@@ -228,22 +241,21 @@ document.addEventListener('keyup', (event) => {
     keys[key].style.backgroundColor = '';
     keys[key].style.transform = '';
   }
-  
+
   if (key === 'ShiftRight' || key === 'ShiftLeft') {
-    updateKeysContent(letters);    
+    updateKeysContent(letters);
     cells[31].style.backgroundColor = '';
     cells[31].style.transform = '';
-    isCapsLockOn = false;   
-} 
+    isCapsLockOn = false;
+  }
 
-if (isCapsLockOn === true) {
+  if (isCapsLockOn === true) {
     cells[31].style.backgroundColor = 'lightblue';
     cells[31].style.transform = 'scale(0.9)';
-}
-
+  }
 });
 
-cells.forEach(cell => {
+cells.forEach((cell) => {
   cell.addEventListener('click', () => {
     const key = cell.textContent;
     if (key === 'Backspace') {
@@ -263,16 +275,14 @@ cells.forEach(cell => {
       textarea.value += key;
     }
 
-    cell.style.backgroundColor = 'lightblue';
-    cell.style.transform = 'scale(0.9)';
+    highlightCell(cell);
 
     if (key === 'CapsLock') {
       isCapsLockOn = !isCapsLockOn;
       if (isCapsLockOn) {
         updateKeysContent(lettersCaps);
       } else {
-        cell.style.backgroundColor = '';
-        cell.style.transform = '';
+        unhighlightCell(cell);
         updateKeysContent(letters);
       }
     }
@@ -284,11 +294,10 @@ cells.forEach(cell => {
       updateKeysContent(letters);
     }
 
-setTimeout(() => {
-  if (key !== 'CapsLock') {
-    cell.style.backgroundColor = '';
-    cell.style.transform = '';
-  }
-}, 300);
-});
+    setTimeout(() => {
+      if (key !== 'CapsLock') {
+        unhighlightCell(cell);
+      }
+    }, 300);
+  });
 });
